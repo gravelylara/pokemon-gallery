b632c164394947ba1aaded03d45dbbf9051d18fc
laragiuliana <lara.giuliana.lima@gmail.com>
2023-01-04 17:17:01 -0300

    Add axios in the project

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,37 @@
   <div>
     <p>This is a test</p>
   </div>
+  <div>
+    {{pokemons}}
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created() {
+    this.getUser();
+  },
+  methods: {
+    getUser() {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=20")
+        .then((res) => {
+          this.pokemons = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
